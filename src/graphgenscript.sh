7a0cea49d2747d058ef87094b30cf8c75e0681da
Theta <1467116498@qq.com>
2023-05-26 21:32:00 +0800

    新增了一些test

--- src/graphgenscript.sh
+++ src/graphgenscript.sh
@@ -1,4 +1,4 @@
 flex lexer.l &\
 bison -d parser.y &\
 g++ main.cpp ast.cpp graphgen.cpp parser.cpp lexer.cpp codegen.cpp utils.cpp -o mcc `llvm-config-12 --libs all --cxxflags --ldflags` &\
-./mcc test.txt -v & dot graph.dot -T png -o graph.png
+./mcc ./testcode/test1.txt -v & dot graph.dot -T png -o graph.png
